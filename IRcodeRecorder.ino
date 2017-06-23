114a9df77ebf5466a29ef68a178d52ba391082fd
Brian Falor <brian.falor@gmail.com>
2017-06-22 23:48:13 -0600

    first commit

---
+++ IRcodeRecorder.ino
@@ -0,0 +1,32 @@
+#include <IRremote.h>
+
+int RECV_PIN = 8;
+IRrecv irrecv(RECV_PIN);
+decode_results results;
+
+void setup()
+{
+  Serial.begin(9600);
+  irrecv.enableIRIn(); // Start the receiver
+}
+int count = 0;
+
+void loop() {
+  if (irrecv.decode(&results)) {
+    Serial.println("results: ");
+    Serial.print("hex ");
+    Serial.println(results.value, HEX);
+    Serial.print("dec ");
+    Serial.println(results.value, DEC); 
+    
+    irrecv.resume(); // Receive the next value
+    delay(200);
+   // count = 0;
+  }
+  
+  else {
+  //  count ++;
+   // Serial.print("count = ");
+   // Serial.println(count);
+  }
+}
